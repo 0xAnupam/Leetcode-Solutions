5e632503d88f81ad3d9d961b3c588e727f843ce5
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-01-17 23:55:20 +0530

    Time: 183 ms (82.12%), Space: 98.6 MB (71.69%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        long long sum=0,n=0;
-            set<pair<long long,int>> s;
-            for(auto &i:nums){
-                    sum+=i;
-                    s.insert({sum,n++});
-             }
-          if(sum==x){
-                  return n;
-                  
-          }
-            if(sum<x){
-                    return -1;
+        long long sum=0;
+        int i=0,j=nums.size()-1,res=nums.size()+1;
+            while(i<=j && sum<x){
+                    sum+=nums[i++];
             }
-            long long res=n;
-            sum=0;
-           
-            for(int i=n-1;i>=0 && sum<=x;i--){
-                    sum+=nums[i];
-                    auto p=s.lower_bound({x-sum,-1});
-                    if(p!=s.end() && (*p).first+sum==x){
-                            res=min(res,1+n-i+(*p).second);
+            if(j<i){
+                    return sum==x ? nums.size():-1;
+            }
+            i--;
+            if(sum==x){
+                    res=i+1;
+            }
+            while(j>=i && j>=0){
+                    sum+=nums[j--];
+                    while(sum>x && i>=0){
+                            sum-=nums[i--];
                     }
                     if(sum==x){
-                            res=min(res,n-i);
+                            res=min(res,i+(int)nums.size()-j);
+                            
                     }
-                    
             }
-            auto p=s.lower_bound({x,0});
-            if(p!=s.end() && (*p).first==x){
-                    res=min((*p).second+1LL,res);
-            }
-            return res==n ? -1:res;
-
+            return res==nums.size()+1? -1:res;
+            
     }
 };
