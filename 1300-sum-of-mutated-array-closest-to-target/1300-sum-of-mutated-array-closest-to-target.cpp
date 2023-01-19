adf48118685840e0aa6a6f0d73155525aeb66503
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-01-20 03:38:05 +0530

    Time: 16 ms (84.99%), Space: 13.2 MB (9.63%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -1,4 +1,13 @@
-
+int f(vector<int> &arr,vector<int>&nums,int i,int n){
+         auto p=upper_bound(arr.begin(),arr.end(),i)-arr.begin();
+                   
+                    int k=nums[p];
+                    if(p<n){
+                            k-=arr[p];
+                    }
+                    k+=(n-p)*i;
+        return k;
+}
 class Solution {
 public:
     int findBestValue(vector<int>& arr, int target) {
@@ -6,27 +15,35 @@ public:
             int n=arr.size();
         vector<int> nums(n);
             int sum=0;
-            int diff=1e9+1,res=0;
+            int diff=1e9+1,res=diff;
             for(int i=0;i<n;i++){
                     sum+=arr[i];
                     nums[i]=sum;
             }
             nums.push_back(sum);
-            int i=0,q=0;
-            while(i<=arr[n-1]){
-                    auto p=upper_bound(arr.begin(),arr.end(),i)-arr.begin();
-                   
-                    int k=nums[p];
-                    if(p<n){
-                            k-=arr[p];
+            int l=0,h=arr[n-1],m;
+            while(l<=h){
+                    m=l+(h-l)/2;
+                    int k=f(arr,nums,m,n);
+                    if(k==target){
+                            return m;
                     }
-                    k+=(n-p)*i;
-                    
+                    if(k<target){
+                            l=m+1;
+                    }
+                    else{
+                            h=m-1;
+                    }
                     if(abs(target-k)<diff){
                             diff=abs(target-k);
-                            res=i;
+                            res=m;
                     }
-                    i++;
+                    if(abs(target-k)==diff){
+                            
+                            res=min(res,m);
+                    }
+                    
+                    
             }
             return res;
     }
