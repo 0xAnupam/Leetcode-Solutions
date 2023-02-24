13d970b3cdc523591eba94f799e795702ae316f3
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-02-24 23:47:34 +0530

    Time: 404 ms (9.90%), Space: 92.4 MB (29.70%) - LeetHub

---
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxSumMinProduct(vector<int>& nums) {
+        stack<pair<int,int>> s;
+        int n=nums.size();
+        vector<int> left(n),right(n);
+        vector<long long> prefix_sum(n);
+            long long sum=0;
+        for(int i=0;i<n;i++){
+                sum+=nums[i];
+                left[i]=i;
+                prefix_sum[i]=sum;
+                s.push({nums[i],i});
+                int l=0;
+                while(s.size() && s.top().first>=nums[i]){
+                        l=s.top().second;
+                        s.pop();
+                }
+                s.push({nums[i],i});
+                left[i]=left[l] ;
+        }
+            stack<pair<int,int>> q;
+            for(int i=n-1;i>=0;i--){
+                    q.push({nums[i],i});
+                    int r=0;
+                    right[i]=i;
+                    while(q.size() && q.top().first>=nums[i]){
+                            r=q.top().second;
+                            q.pop();
+                    }
+                    q.push({nums[i],i});
+                    right[i]=right[r];
+            }
+            long long res=0,mod=1e9+7;
+            for(int i=0;i<n;i++){
+                   
+                    sum=prefix_sum[right[i]]-prefix_sum[left[i]]+nums[left[i]];
+                    sum*=(1LL*nums[i]);
+                    res=max(res,sum);
+            }
+            res%=mod;
+            return res;
+            
+    }
+};
