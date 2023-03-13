8605dd7606bd30426ef88b902851ebb727e76d13
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-03-13 16:59:43 +0530

    Added solution - LeetHub

--- Remove minimum number of elements - GFG/remove-minimum-number-of-elements.cpp
+++ Remove minimum number of elements - GFG/remove-minimum-number-of-elements.cpp
@@ -10,19 +10,17 @@ using namespace std;
 class Solution{
   public:
     int minRemove(int a[], int b[], int n, int m) {
-        const int N=1e5+1;
-        int fa[N],fb[N];
-        memset(fa,0,sizeof(fa));
-        memset(fb,0,sizeof(fb));
+        unordered_map<int,int> fa,fb;
         for(int i=0;i<n;i++){
             fa[a[i]]++;
         }
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++)
+        {
             fb[b[i]]++;
         }
         int res=0;
-        for(int i=0;i<N;i++){
-            res+=min(fa[i],fb[i]);
+        for(auto &i:fa){
+            res+=min(i.second,fb[i.first]);
         }
         return res;
     }
