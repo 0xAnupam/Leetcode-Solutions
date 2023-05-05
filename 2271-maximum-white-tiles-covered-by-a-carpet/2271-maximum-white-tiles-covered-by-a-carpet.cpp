9e4cac1694ce0ca38b062992c2bf1d49f348eb1f
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-05-05 16:10:54 +0530

    Time: 520 ms (10.55%), Space: 79.8 MB (8.89%) - LeetHub

---
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
+       sort(tiles.begin(),tiles.end());
+            
+       int res=0,n=tiles.size();
+       int arr[n],sum[n];     
+       for(int i=0;i<n;i++){
+               arr[i]=(tiles[i][1]-tiles[i][0]+1);
+               sum[i]=arr[i];
+               if(i){
+                       sum[i]+=sum[i-1];
+               }
+       }
+       for(int i=0;i<n;i++){
+               vector<int> t={tiles[i][0]+carpetLen,0};
+               auto it=lower_bound(tiles.begin()+i,tiles.end(),t)-tiles.begin();
+               it--;
+               int q=sum[it];
+               if(i){
+                       q-=sum[i-1];
+               }
+               q-=arr[it];
+               q+=(min(tiles[i][0]+carpetLen-1,tiles[it][1])-tiles[it][0]+1);
+               // cout<<i<<'\t'<<q<<'\n';
+               res=max(res,q);
+               
+       }     return res;
+    }
+};
