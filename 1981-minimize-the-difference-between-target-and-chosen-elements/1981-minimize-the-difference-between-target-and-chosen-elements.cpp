b25d4d7321b53d1e5c857a7850c56c9a6698bf1d
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-08-05 02:06:31 +0530

    Time: 1220 ms (69.69%), Space: 41.3 MB (35.75%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,31 @@
+const int N=71;
+int8_t dp[N+10][N*N*N+1];
+void dfs(vector<vector<int>> &arr,int idx,int n,int m,int sum){
+    
+    if(dp[idx][sum]){
+        return ;
+    }
+    if(idx==n){
+        dp[idx][sum]=1;
+        return ;
+    }
+    for(int i=0;i<m;i++){
+        dfs(arr,idx+1,n,m,sum+arr[idx][i]);
+    }
+    dp[idx][sum]=1;
+}
+class Solution {
+public:
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        memset(dp,0,sizeof(dp));
+        int n=mat.size(),m=mat[0].size();
+        dfs(mat,0,n,m,0);
+        int res=1e9;
+        for(int i=0;i<800*n+1;i++){
+            if(dp[n][i]){
+                res=min(res,abs(target-i));
+            }
+        }
+            return res;
+    }
+};
