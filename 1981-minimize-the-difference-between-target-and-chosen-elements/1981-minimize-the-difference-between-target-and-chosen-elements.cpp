067ba5310bad0b7cc31ea3f2ad826bda41148def
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-08-05 02:10:08 +0530

    Time: 1200 ms (70.73%), Space: 36.7 MB (35.88%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,5 +1,5 @@
-const int N=71;
-int8_t dp[N+10][N*N*N+1];
+const int N=70;
+int8_t dp[N+1][N*N*N+1];
 void dfs(vector<vector<int>> &arr,int idx,int n,int m,int sum){
     
     if(dp[idx][sum]){
@@ -19,6 +19,10 @@ public:
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         memset(dp,0,sizeof(dp));
         int n=mat.size(),m=mat[0].size();
+        for(auto &i:mat){
+                sort(i.begin(),i.end());
+        }
+        sort(mat.begin(),mat.end());
         dfs(mat,0,n,m,0);
         int res=1e9;
         for(int i=0;i<800*n+1;i++){
