cb1d6cc2450224105a8a3ae21364d1e70c9d8fef
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-08-12 00:05:46 +0530

    Time: 1124 ms (72.99%), Space: 13.5 MB (95.11%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,32 +1,29 @@
-const int N=70;
-int8_t dp[N*N*N+1][N+1];
+const int N=71;
+int8_t dp[N][N*N];
 void dfs(vector<vector<int>> &arr,int idx,int n,int m,int sum){
     
-    if(dp[sum][idx]){
+    if(dp[idx][sum]){
         return ;
     }
     if(idx==n){
-        dp[sum][idx]=1;
+        dp[idx][sum]=1;
         return ;
     }
     for(int i=0;i<m;i++){
         dfs(arr,idx+1,n,m,sum+arr[idx][i]);
     }
-    dp[sum][idx]=1;
+    dp[idx][sum]=1;
 }
 class Solution {
 public:
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         memset(dp,0,sizeof(dp));
         int n=mat.size(),m=mat[0].size();
-        for(auto &i:mat){
-                sort(i.begin(),i.end());
-        }
-        sort(mat.begin(),mat.end());
         dfs(mat,0,n,m,0);
         int res=1e9;
-        for(int i=0;i<800*n+1;i++){
-            if(dp[i][n]){
+        int k=min(800*n,N*N-1); 
+        for(int i=0;i<=k;i++){
+            if(dp[n][i]){
                 res=min(res,abs(target-i));
             }
         }
