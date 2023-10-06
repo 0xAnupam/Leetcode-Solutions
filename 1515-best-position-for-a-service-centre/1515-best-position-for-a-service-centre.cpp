e8a85b0ca0922c317b39285c03683af6d0d5df5e
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-10-07 02:21:54 +0530

    Time: 11 ms (54.84%), Space: 8 MB (40.86%) - LeetHub

---
+++ 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.cpp
@@ -0,0 +1,111 @@
+struct Point {
+    double x, y;
+};
+
+Point test_point[] = { { -1.0, 0.0 },
+                       { 0.0, 1.0 },
+                       { 1.0, 0.0 },
+                       { 0.0, -1.0 } };
+
+double lower_limit = 0.00001;
+
+double distSum(Point p,Point arr[], int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        double distx = abs(arr[i].x - p.x);
+        double disty = abs(arr[i].y - p.y);
+        sum += sqrt((distx * distx) + (disty * disty));
+    }
+    return sum;
+}
+
+double geometricMedian(Point arr[], int n)
+{
+ 
+    Point current_point;
+ 
+    for (int i = 0; i < n; i++) {
+        current_point.x += arr[i].x;
+        current_point.y += arr[i].y;
+    }
+ 
+    current_point.x /= n;
+    current_point.y /= n;
+ 
+    
+    double minimum_distance = 
+       distSum(current_point, arr, n);
+ 
+    int k = 0;
+    while (k < n) {
+        for (int i = 0; i < n, i != k; i++) {
+            Point newpoint;
+            newpoint.x = arr[i].x;
+            newpoint.y = arr[i].y;
+            double newd = 
+                   distSum(newpoint, arr, n);
+            if (newd < minimum_distance) {
+                minimum_distance = newd;
+                current_point.x = newpoint.x;
+                current_point.y = newpoint.y;
+            }
+        }
+        k++;
+    }
+ 
+    // Assume test_distance to be 1000
+    double test_distance = 1000;
+    int flag = 0;
+ 
+    // Test loop for approximation starts here
+    while (test_distance > lower_limit) {
+ 
+        flag = 0;
+ 
+        // Loop for iterating over all 4 neighbours
+        for (int i = 0; i < 4; i++) {
+ 
+            // Finding Neighbours done
+            Point newpoint;
+            newpoint.x = current_point.x
+                 + (double)test_distance * test_point[i].x;
+            newpoint.y = current_point.y
+                 + (double)test_distance * test_point[i].y;
+ 
+            // New sum of Euclidean distances
+            // from the neighbor to the given
+            // data points
+            double newd = distSum(newpoint, arr, n);
+ 
+            if (newd < minimum_distance) {
+ 
+                // Approximating and changing
+                // current_point
+                minimum_distance = newd;
+                current_point.x = newpoint.x;
+                current_point.y = newpoint.y;
+                flag = 1;
+                break;
+            }
+        }
+ 
+       
+        if (flag == 0)
+            test_distance /= 2;
+    }
+ 
+   return minimum_distance;
+}
+class Solution {
+public:
+    double getMinDistSum(vector<vector<int>>& positions) {
+            int n=positions.size();
+        Point arr[n];
+            for(int i=0;i<n;i++){
+                    arr[i].x=positions[i][0];
+                    arr[i].y=positions[i][1];
+            }
+            return geometricMedian(arr,n);
+    }
+};
