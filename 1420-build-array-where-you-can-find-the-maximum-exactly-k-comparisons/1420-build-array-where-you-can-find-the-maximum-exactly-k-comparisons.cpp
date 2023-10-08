0b49480a9ae88103ca700f1c7da95222649ae565
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-10-08 16:03:55 +0530

    Time: 69 ms (63.64%), Space: 7.4 MB (65.00%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,26 @@
+const int mod=1e9+7;
+int dp[51][101][51];
+int f(int idx,int n,int m,int k,int p){
+        if(idx==n){
+                return k==0;
+        }
+        if(k<0){
+                return 0;
+        }
+        if(dp[idx][p][k]!=-1){
+                return dp[idx][p][k];
+        }
+        int res=0;
+        for(int i=1;i<=m;i++){
+                res+=f(idx+1,n,m,k-(i>p),max(i,p));
+                res%=mod;
+        }
+        return dp[idx][p][k]=res;
+}
+class Solution {
+public:
+    int numOfArrays(int n, int m, int k) {
+            memset(dp,-1,sizeof(dp));
+        return f(0,n,m,k,0);
+    }
+};
