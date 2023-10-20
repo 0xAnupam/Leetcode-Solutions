6eff61f85c1ab38c532afa6a22ca6b86b9a40c6d
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-10-20 14:02:12 +0530

    Time: 153 ms (12.87%), Space: 91.7 MB (5.04%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        int n=heights.size();
+            vector<int>nxt(n),smoll(n);
+            stack<pair<int,int>> s,st;
+            s.push({INT_MAX,n});
+            for(int i=n-1;i>=0;i--){
+                    while(s.top().first<heights[i]){
+                            s.pop();
+                    }
+                    nxt[i]=s.top().second;
+                    s.push({heights[i],i});
+            }
+            
+            
+            vector<int> res(n);
+            for(int i=0;i<n;i++){
+                    
+                    int j=nxt[i],cnt=0,p=0;
+                    if(i<n-1 && heights[i+1]<heights[i]){
+                            j=i+1;
+                    }
+                    while(j<n && heights[i]>p){
+                            p=heights[j];
+                            j=nxt[j];
+                            cnt++;
+                            
+                    }
+                    res[i]=cnt;
+            }
+            return res;
+    }
+};
