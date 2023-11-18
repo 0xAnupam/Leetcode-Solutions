e14147cf7ddf5b357ead43e286bf3bc16d94b4c4
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-11-18 15:38:38 +0530

    Time: 251 ms (7.11%), Space: 99.4 MB (43.97%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,32 +1,20 @@
 
-long long Sum(vector<long long> &adj,int l,int r){
-        
-        return l ? adj[r]-adj[l-1]: adj[r];
-}
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+        sort(nums.rbegin(),nums.rend());
         long long sum=0,n=nums.size();
-            vector<long long> adj;
-            for(auto &i:nums){
-                    sum+=i;
-                    adj.push_back(sum);
-            }
-            long long res=1;
-            for(long long i=0;i<n-res;i++){
-                    long long l=i+1,h=n-1,m;
-                    while(l<=h){
-                            m=l+(h-l)/2;
-                            if((nums[m]*(m-i+1))-Sum(adj,i,m)<=k){
-                                    l=m+1;
-                            }
-                            else{
-                                    h=m-1;
-                            }
+            int res=0,i=0,j=0;
+            while(i<n){
+                    while(j<n && (j-i+1)*1LL*nums[i]-sum-nums[j]<=k){
+                            sum+=nums[j];
+                            j++;
+                            res=max(res,j-i);
                     }
-                    res=max(res,l-i);
+                    sum-=nums[i];
+                    i++;
             }
+            
             return res;
             
     }
