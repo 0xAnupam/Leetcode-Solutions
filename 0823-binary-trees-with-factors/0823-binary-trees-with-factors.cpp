ce2a9c808d36194a4b89d88e7de3380c8fa6b64d
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2023-11-18 16:17:16 +0530

    Time: 142 ms (6.17%), Space: 10.3 MB (31.18%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
@@ -0,0 +1,46 @@
+const int mod=1e9+7;
+const int N=1e3+1;
+int dp[N];
+int f(int idx,vector<int>* adj){
+        if(dp[idx]!=-1){
+                return dp[idx];
+        }
+        int res=1,n=adj[idx].size();
+        for(int i=0;i<n;i+=2){
+                int Gg=(f(adj[idx][i],adj)*1LL*f(adj[idx][i+1],adj))%mod;
+                res+=Gg;
+                res%=mod;
+                if(adj[idx][i]!=adj[idx][i+1]){
+                        res+=Gg;
+                        res%=mod;
+                }
+        }
+        return dp[idx]=res;
+}
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int n=arr.size();
+            vector<int> adj[n];
+            unordered_map<long long,int> mp;
+            for(int i=0;i<n;i++){
+                    dp[i]=-1;
+                    mp[arr[i]]=i;
+            }
+            for(int i=0;i<n;i++){
+                    for(int j=i;j<n;j++){
+                            long long p=arr[i]*1LL*arr[j];
+                            if(mp.find(p)!=mp.end()){
+                                    adj[mp[p]].push_back(i);
+                                    adj[mp[p]].push_back(j);
+                            }
+                    }
+            }
+            int res=0;
+            for(int i=0;i<n;i++){
+                    res+=f(i,adj);
+                    res%=mod;
+            }
+            return res;
+    }
+};
