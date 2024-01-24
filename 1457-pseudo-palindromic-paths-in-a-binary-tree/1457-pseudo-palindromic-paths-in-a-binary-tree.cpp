d9fbd1de97c7d3fc01262c35a74a9dd020ff53e3
theanupam1nonly <114435894+theanupam1nonly@users.noreply.github.com>
2024-01-24 22:23:16 +0530

    Time: 250 ms (85.52%), Space: 176.9 MB (99.73%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -9,32 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- bool pos(vector<int>&f){
-        int o=0;
-        for(auto &i:f){
-                if(i&1){
-                        o++;
-                        if(o>1){return 0;}
-                }
-        }
-        return 1;
-}
-int pal(TreeNode* root,vector<int> f){
+int pal(TreeNode* root,int f){
         if(root==NULL){
                 return 0;
         }
-        f[root->val]++;
+        f^=(1<<(root->val));
         if(root->left || root->right){
                 return pal(root->left,f)+pal(root->right,f);
         }
-        return pos(f)? 1:0;
+        return (f&(f-1)) ==0 ;
 }
 class Solution {
 public:
         
     int pseudoPalindromicPaths (TreeNode* root) {
-        vector<int> f(10,0);
-            return pal(root,f);
-            
+            return pal(root,0);
     }
 };
