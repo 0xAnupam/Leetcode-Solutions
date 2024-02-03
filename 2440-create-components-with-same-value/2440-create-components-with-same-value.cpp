755b8b62008fef9d78bdf7f407a3cdf0ce565d66
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-02-04 03:41:20 +0530

    Time: 342 ms (75.26%), Space: 122 MB (100.00%) - LeetHub

--- 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
@@ -1,6 +1,5 @@
 int flag;
-const int N=2e4+1;
-vector<int> m[N];
+// vector<int> m[N];
     int dfs(int u,int p,vector<int> *m,vector<int> &nums,int k){
         int sum=nums[u];
         for(auto &x:m[u]){
@@ -17,18 +16,41 @@ vector<int> m[N];
          return 0;
         return sum;
     }
+vector<pair<int,int>> ff(int n){
+    vector<pair<int,int>> res;
+        for(int i=2;i*i<=n;i+=2){
+                if(n%i==0){
+                        int cnt=0;
+                        while(n>1 && n%i==0){
+                                cnt++;
+                                n/=i;
+                        }
+                        res.push_back({i,cnt});
+                }
+                if(i==2)i--;
+        }
+        if(n>1){
+                res.push_back({n,1});
+        }
+    return res;
+}
+void factors(vector<pair<int,int>> &arr,int idx,int cur,vector<int> &ans){
+    if(idx==arr.size()){
+        ans.push_back(cur);
+        return ;
+    }
+    factors(arr,idx+1,cur,ans);
+    int mult=1;
+    for(int j=1;j<=arr[idx].second;j++){
+        mult*=arr[idx].first;
+        factors(arr,idx+1,cur*mult,ans);
+    }
+}
 class Solution {
 public:
     
     int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
         int n = nums.size();
-            // for(auto &i:m){
-            //         while(i.second.size()){
-            //                 i.second.pop_back();
-            //         }
-            // }
-            // m->clear();
-        // map <int,vector<int>> m;
             vector<int> m[n];
         for(auto &x:edges)
         {
@@ -38,15 +60,25 @@ public:
         int sum=0;
         for(auto &x:nums) sum+=x;
         int ans=0;
-        for(int i=1;i<=sum;i++){
-            if(sum%i==0){
-                flag=1;
+        // for(int i=1;i<=sum;i++){
+        //     if(sum%i==0){
+        //         flag=1;
+        //         dfs(0,-1,m,nums,sum/i);
+        //         if(flag==1){
+        //             ans=max(ans,i-1);
+        //         }
+        //     }
+        // }
+            auto p=ff(sum);
+            vector<int> fact;
+            factors(p,0,1,fact);
+            for(auto &i:fact){
+                    flag=1;
                 dfs(0,-1,m,nums,sum/i);
                 if(flag==1){
                     ans=max(ans,i-1);
                 }
             }
-        }
         return ans;
     }
 };
