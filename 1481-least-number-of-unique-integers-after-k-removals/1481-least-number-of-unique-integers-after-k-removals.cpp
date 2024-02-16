19d4726e597936a17b6b727a192d25c7d8abab25
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-02-16 17:38:47 +0530

    Time: 82 ms (98.61%), Space: 49.2 MB (97.45%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -6,14 +6,10 @@ public:
             int i=0,n=arr.size();
             while(i<n){
                     int tmp=arr[i],cnt=0;
-                    while(i<n && arr[i]==tmp){
-                            i++;
-                            cnt++;
-                    }
+                    while(i<n && arr[i]==tmp)i++,cnt++;
                     p.emplace_back(cnt);
             }
-            n=p.size();
-            i=0;
+            i=0,n=p.size();
             sort(p.begin(),p.end());
             while(i<n && p[i]<=k){
                     k-=p[i++];
