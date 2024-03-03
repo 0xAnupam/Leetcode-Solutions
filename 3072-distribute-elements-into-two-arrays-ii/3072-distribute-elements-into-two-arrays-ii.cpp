b27ba00aab395a349e2baa74442f74d426871289
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-03-03 11:56:28 +0530

    Time: 643 ms (12.50%), Space: 193.7 MB (12.50%) - LeetHub

---
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
@@ -0,0 +1,41 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        pbds A,B;
+            vector<int> arr1,arr2;
+            for(auto &i:nums){
+                    if(arr1.empty()){
+                            arr1.emplace_back(i);
+                            A.insert(i);
+                    }
+                    else if(arr2.empty()){
+                            arr2.emplace_back(i);
+                            B.insert(i);
+                    }
+                    else if(A.order_of_key(i)>B.order_of_key(i)){
+                            arr1.emplace_back(i);
+                            A.insert(i);
+                    }
+                    else if(A.order_of_key(i)<B.order_of_key(i)){
+                            arr2.emplace_back(i);
+                            B.insert(i);
+                    }
+                    else if(A.size()<=B.size()){
+                            arr1.emplace_back(i);
+                            A.insert(i);
+                    }
+                    else{
+                            arr2.push_back(i);
+                            B.insert(i);
+                    }
+            }
+            arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+            return arr1;
+    }
+};
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
