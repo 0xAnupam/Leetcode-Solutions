cd3156a8a4677b2e64ef102aa61612638161d8d4
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-03-20 21:14:42 +0530

    Time: 609 ms (16.43%), Space: 22.6 MB (23.65%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
+        unordered_map<char,unordered_set<char>> mps;
+        for(auto mping : mappings) 
+            mps[mping[0]].insert(mping[1]);
+        
+        int n = s.size(),m = sub.size();
+        
+        for(int i=0;i<= n-m;i++){
+            int k;
+            for(k=0;k<m;k++){
+                if(s[i+k] != sub[k] and mps[sub[k]].find(s[i+k]) == mps[sub[k]].end()) break;
+            }
+            
+            if(k == m) return true;
+        }
+        
+        return false;
+    }
+};
