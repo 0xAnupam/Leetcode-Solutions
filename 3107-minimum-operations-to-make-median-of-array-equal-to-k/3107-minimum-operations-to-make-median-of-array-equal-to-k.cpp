030218109e4fa7c2231794c1329f608bede78ab5
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-04-25 17:44:57 +0530

    Time: 1069 ms (5.31%), Space: 243.5 MB (6.34%) - LeetHub

--- 3107-minimum-operations-to-make-median-of-array-equal-to-k/3107-minimum-operations-to-make-median-of-array-equal-to-k.cpp
+++ 3107-minimum-operations-to-make-median-of-array-equal-to-k/3107-minimum-operations-to-make-median-of-array-equal-to-k.cpp
@@ -2,11 +2,6 @@
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-void myerase(pbds &t, int v){
-    int rank = t.order_of_key(v);//Number of elements that are less than v in t
-    pbds::iterator it = t.find_by_order(rank); //Iterator that points to the (rank+1)th element in t
-    t.erase(it);
-}
 class Solution {
 public:
     long long minOperationsToMakeMedianK(vector<int>& nums, int k) {
@@ -14,9 +9,8 @@ public:
             long long res=0,n=nums.size();
             while(*A.find_by_order(n/2)!=k){
                     res+=abs(*A.find_by_order(n/2)-k);
-                    myerase(A,*A.find_by_order(n/2));
+                    A.erase(A.find_by_order(n/2));
                     A.insert(k);
-                    
             }
             return res;
             
