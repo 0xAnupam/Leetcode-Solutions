948051fe2e38cef3361331b3a2b4803a3246298a
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-08-03 01:48:18 +0530

    Time: 88 ms (12.43%), Space: 77.6 MB (5.10%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,8 +1,16 @@
+int fun(vector<int>& nums , int index  , int n , vector<int> &dp){
+    if(index==n)return 0;
+    if(dp[index]!=INT_MIN)return dp[index];
+    int res=fun(nums,index+1,n,dp);
+    return dp[index]=nums[index]+max(0,res);
+}
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans=INT_MIN,sum=0;
-        for(auto &i:nums)sum+=i,ans=max(ans,sum),sum=max(sum,0);
-        return ans;
+        int n=nums.size();
+        int res=INT_MIN;
+        vector<int> dp(n,INT_MIN);
+        for(int i=0;i<n;i++)res=max(res,fun(nums,i,n , dp));
+        return res;
     }
 };
