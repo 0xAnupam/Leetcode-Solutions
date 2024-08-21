5603b9e56f188920023558859924bd68417e3062
0xAnupam <114435894+0xAnupam@users.noreply.github.com>
2024-08-21 16:56:47 +0530

    Time: 166 ms (82.85%), Space: 40.3 MB (85.33%) - LeetHub

--- 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
+++ 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
@@ -1,74 +1,33 @@
-const int mod=1e9+7;
+const int mod = 1e9 + 7;
 int dp[2001][1001];
-int  Seg_Tree[4269];
-// const int maxA=1002;
-void build(int x,int l,int r){
-    if(r-l==1){
-        Seg_Tree[x]=1;
-        return ;
-    }
-    int mid=l+(r-l)/2;
-    build(2*x+1,l,mid);
-    build(2*x+2,mid,r);
-    Seg_Tree[x]=(Seg_Tree[2*x+1]+Seg_Tree[2*x+2])%mod;
-}
-void update(int x,int idx,int val,int l,int r){
-    if(r-l==1){
-        Seg_Tree[x]=val;
-        return ;
-    }
-    int m=l+(r-l)/2;
-    if(idx<m){
-        update(2*x+1,idx,val,l,m);
-    }
-    else{
-        update(2*x+2,idx,val,m,r);
-    }
-    Seg_Tree[x]=(Seg_Tree[2*x+1]+Seg_Tree[2*x+2])%mod;
-}
-int query(int x,int a,int b,int l,int r){
-   
-    if(a>=r || b<=l){
-        return 0;
-    }
-    else if(a<=l && b>=r){
-        return Seg_Tree[x];
-    }
-    int mid=l+(r-l)/2;
-    auto left=query(2*x+1,a,b,l,mid);
-    auto right=query(2*x+2,a,b,mid,r);
-    return (left+right)%mod;
-}
+
 class Solution {
 public:
     int countOfPairs(vector<int>& nums) {
-        int x=nums.back(),n=nums.size();
-        vector<int> arr=nums;
+        int x = nums.back(), n = nums.size();
+        vector<int> arr = nums;
         arr.push_back(x);
-        for(int i=n-1;i>=0;i--){
-            x=min(x,nums[i]);
-            nums[i]=x;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            x = min(x, nums[i]);
+            nums[i] = x;
         }
-        int maxA=*max_element(arr.begin(),arr.end())+1;
-        build(0,0,maxA);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=arr[i];j++)dp[i][j]=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= arr[i]; j++) dp[i][j] = 0;
         }
-        for(int i=n-1;i>=0;i--){
-            for(int cur=0;cur<=nums[i];cur++){
-                int l=max(arr[i+1]-arr[i]+cur,cur),r=arr[i+1];
-                
-                    dp[i][cur]=query(0,l,r+1,0,maxA);
-                    // dp[i][cur]%=mod;
-              
+        for (int i = 0; i <= arr[n]; i++) dp[n][i] = 1+i;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int cur = 0; cur <= nums[i]; cur++) {
+                int l = max(arr[i + 1] - arr[i] + cur, cur);
+                int r = arr[i + 1];
+                dp[i][cur] = dp[i + 1][r];
+                if (l > 0) dp[i][cur] = (dp[i][cur] - dp[i + 1][l - 1] + mod) % mod;
             }
-            for(int cur=0;cur<=arr[i];cur++){
-                update(0,cur,dp[i][cur],0,maxA);
+            for (int cur = 1; cur <= arr[i]; cur++) {
+                dp[i][cur] += dp[i][cur - 1];
+                dp[i][cur] %= mod;
             }
-            
         }
         return dp[0][0];
-    
-        
     }
 };
